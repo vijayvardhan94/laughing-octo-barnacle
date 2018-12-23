94d36fe54c05d6d3304018df885bc696004c7e9a
Vijay Tadimeti <tadimetv@oregonstate.edu>
2018-12-23 01:50:30 -0800

    par

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 100,
+        "value": 150,
         "density": {
           "enable": true,
           "value_area": 1000
